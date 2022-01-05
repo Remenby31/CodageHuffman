42534f5eb9551aaf1ac240c962c6fb4d63c55964
cassmussard <84839641+cassmussard@users.noreply.github.com>
2022-01-05 16:45:40 +0100

    Update Huffman.adb

--- Huffman.adb
+++ Huffman.adb
@@ -14,7 +14,25 @@ package body codagehuffman is
         return Tableau;
     end Calcul_Frequence;
 
-    681957
+procedure Tri_selection(Tableau : in out T_Tableau) is
+        minimum : Integer;
+        Tampon : T_Cellule;
+    begin
+        for I in 1..256 loop
+            minimum := I;
+            for J in 1..256 loop
+                if Tableau(I).all.Donnee > Tableau(J).all.Donnee then
+                    minimum := J;
+                else
+                    null;
+                end if;
+                end loop;
+                Tampon := Tableau(I);
+                Tableau(I) := Tableau(minimum);
+                Tableau(minimum) := Tampon;
+        end loop;
+    end Tri_selection;
+
     procedure Construire_Arbre(Tableau : in T_Tableau) is
 
         Tableau_Construction : T_Tableau;
