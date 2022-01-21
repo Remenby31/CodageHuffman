526424358f0bf3195a0730f04f06bc5ef03a09a7
cassmussard <84839641+cassmussard@users.noreply.github.com>
2022-01-21 19:59:54 +0100

    final

--- src/codagehuffman.ads
+++ src/codagehuffman.ads
@@ -1,38 +1,32 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Command_line; use Ada.Command_line;
+with Ada.Sequential_IO ;	-- pour l'accès typé aux fichiers (integer, naturel,							-- float, etc.)
+with Text_Io;   use Text_Io;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with cellule;
+with Arbre_Exceptions;         use Arbre_Exceptions;
+with Arbre;
 
-package codagehuffman is
+package codageHuffman is
+   type T_Tableau is private;
+   type T_byte is mod 2**8;
 
-    package Cellule_Huffman is
-            new Cellule(T_Cle => Character, T_Donnee => Integer);
-    use Cellule_Huffman;
-
-    type T_Tableau is limited private;
+   package Byte_file is new Ada.Sequential_IO(T_byte) ;
+   use Byte_file ;
 
 
-    -- Calculer les fr�quences des caract�res du texte
-    function Calcul_Frequence(texte : in String) return T_Tableau;
-    --Trier par ordre croissant le tableau de fr�quence
-    function Tri(Tableau : in out T_Tableau; premier : in Integer; dernier : in Integer) return Integer;
+   -- Compresser le fichier
+   procedure Compresser_ficher(nom_fichier : in String; Bool_afficher_Arbre : in Boolean);
 
-    procedure  Tri_rapide(Tableau : in out T_Tableau ; premier : in Integer; dernier : in Integer);
-
-    -- Construire l'arbre de Huffman gr�ce aux fr�quences des caract�res
-    function Construire_Arbre(Tableau : in T_Tableau) return T_Cellule;
-
-    -- Décompresser le fichier
-    function Decompresser_fichier(texte : in String) return String;
-
-    -- Compresser le fichier
-    function Compresser_ficher(texte : in String) return String;
-
-    generic
-        with procedure Traiter(Frequence : in Integer; Caractere : in Character);
-    procedure Parcours_infixe(Tableau : in T_Tableau);
+   -- D�compresser le fichier
+   procedure Decompresser_fichier(nom_fichier : in String);
 
 private
 
-    type T_Tableau is array(0..256) of Integer;
+   package Arbre_codageHuffman is
+     new Arbre(T_Cle => Unbounded_String, T_Donnee => Integer);
+   use Arbre_codageHuffman;
 
+   type T_Tableau is array(1..257) of T_Arbre;
 
 end codagehuffman;
